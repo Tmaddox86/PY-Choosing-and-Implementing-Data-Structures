e10b52c265f3d89806e6b5c01d230c9f011f848b
Tara Maddox <TMaddox85@gmail.com>
2023-06-05 15:43:54 -0700

    first commit

--- arrays/3.py
+++ arrays/3.py
@@ -1,1 +1,15 @@
+# Use a single operation (no loops) to multiply every element of an array by 3.
 
+import numpy as np
+a = np.array([1,2,3,4,5])
+
+#arr = [1, 2, 3, 4, 5]
+#arr = [i * 3 for i in arr]
+#print(arr)
+
+
+
+def multiply_elements_by_3(arr):
+    return 3 * arr
+
+multiply_elements_by_3(a)
